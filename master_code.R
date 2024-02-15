ac0611a58347bfe6c3f0387e3ea5896a6020cb39
Harrah Friedlander <h.friedlander@uq.net.au>
2024-02-15 11:50:47 +1000

    subset data to Acrobates

--- master_code.R
+++ master_code.R
@@ -55,7 +55,8 @@ library(tidyverse)
 library(boilrdata)
 
 
-marsupialsTibble <- as_tibble(marsupials)
+marsupialsTibble <- as_tibble(marsupials) |>
+  filter(Genus == "Acrobates")
 
 
 summary(lm(MaxLifeSpan ~ Mass, data = marsupials))
